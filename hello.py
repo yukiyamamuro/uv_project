e9c8c0308888236ae4e1554c2b3d4ff8b9a4435c
YamamuroY <yuki.sumaho@gmail.com>
2024-10-13 22:10:28 +0900

    chore: fix lint && add vscode extention

--- hello.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from uv-proj!zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz")
-
-
-if __name__ == "__main__":
-    main()
