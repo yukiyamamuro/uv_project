a6f2bdffdb7e9cde1ee7f4dfefc87bb3efeb7dd8
YamamuroY <yuki.sumaho@gmail.com>
2024-10-13 22:02:48 +0900

    chore: add ruff && pre-commit

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 def main():
-    print("Hello from uv-proj!")
+    print("Hello from uv-proj!zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz")
 
 
 if __name__ == "__main__":
